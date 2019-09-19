33d008cce9702cc1a134929fbda824ec55650582
link1d <sebas.mz@gmail.com>
2019-09-19 04:25:43 +0000

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main (void){
+/**
+ * main - puts
+ *
+ * Return: Always 0 (Succes)
+ */
+int main (void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
