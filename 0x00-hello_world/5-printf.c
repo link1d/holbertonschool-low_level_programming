267e4b2edd222c2b1d359188dc4f5fc999ea8456
link1d <sebas.mz@gmail.com>
2019-09-19 14:51:36 +0000

    Print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - pintf command
  *
@@ -7,6 +8,7 @@
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art, \n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
 }
