04dc494a814a8e3ec2475a43f0c9814eed844c6d
link1d <sebas.mz@gmail.com>
2019-09-20 03:17:26 +0000

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Display Numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+		printf("%d", a);
+
+	printf("/n");
+
+	return (0);
+}
