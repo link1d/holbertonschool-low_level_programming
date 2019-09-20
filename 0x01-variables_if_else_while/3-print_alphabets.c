1d0a0baa608db222d3cafe0c3ebdaa3b1a216722
link1d <sebas.mz@gmail.com>
2019-09-20 01:15:44 +0000

    alphaBETS

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - alphabets ALPHABETS
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		putchar(a);
+
+	for (a = 'A'; a <= 'Z'; a++)
+		putchar(a);
+
+	putchar('\n');
+
+	return (0);
+}
