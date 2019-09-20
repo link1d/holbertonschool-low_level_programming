f18aef9b046ac87596423e163c767ff22f545979
link1d <sebas.mz@gmail.com>
2019-09-20 20:32:03 +0000

    Comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,15 +9,16 @@ int main(void)
 {
 	int a;
 
-	for (a = 0; a <= 9; a++)
+	for (a = '0'; a <= '9'; a++)
 	{
 		putchar(a);
-		if (a != 9)
+		if (a != '9')
 		{
 			putchar(',');
 			putchar(' ');
 		}
 	}
 		putchar('\n');
+
 		return (0);
 }
