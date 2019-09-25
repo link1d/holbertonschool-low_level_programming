301d3b80d4e064b05df0d27c7cda91b07433bcc3
link1d <sebas.mz@gmail.com>
2019-09-25 02:31:15 +0000

    Lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _islower - Check lower case
+ *
+ * Return: 1 for lower. 0 for else
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	return (0);
+}
